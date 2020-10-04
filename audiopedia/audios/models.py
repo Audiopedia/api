79c9875cedc7d15c2560d3d38c87c2a61adb7567
Ben Wu <benjaminwu@college.harvard.edu>
2020-10-03 22:06:31 -0400

    mutations for create/update/delete

--- audiopedia/audios/models.py
+++ audiopedia/audios/models.py
@@ -10,14 +10,45 @@ from django.db import models
 * language
 * category
 * actual audio file path??
-
 from audios.models import Audio
 Audio.objects.create(question="How can cleanliness prevent sickness?", language="en", category="Cleanliness")
 Audio.objects.create(question="What drugs should I avoid to stay healthy?", language="en", category="Substance Abuse")
 Audio.objects.create(question="Which helper foods should I eat regularly?", language="en", category="Nutrition")
-"""
-
 class Audio(models.Model):
     question = models.TextField(blank=True)
     language = models.TextField(blank=True)
     category = models.TextField(blank=True)
+"""
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+    audio_url = models.URLField()
+    published = models.BooleanField(default=True)
+
+class Track(models.Model):
+    title = models.CharField(max_length=200)
+    index = models.IntegerField(unique=True)
+    audio_url = models.URLField()
+    transcript = models.TextField()
+    duration = models.DurationField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    active = models.BooleanField(default=True)
+    published = models.BooleanField(default=True)
+
+class Playlist(models.Model):
+    title = models.CharField(max_length=200)
+    index = models.IntegerField(unique=True)
+    audio_url = models.URLField()
+    active = models.BooleanField(default=True)
+    published = models.BooleanField(default=True)
+    tracks = models.ManyToManyField(Track)
+
+class Topic(models.Model):
+    title = models.CharField(max_length=200)
+    index = models.IntegerField(unique=True)
+    audio_url = models.URLField()
+    active = models.BooleanField(default=True)
+    published = models.BooleanField(default=True)
+    playlists = models.ManyToManyField(Playlist)
--- audiopedia/audios/schema.py
+++ audiopedia/audios/schema.py
@@ -2,14 +2,267 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from .models import Audio
+from .models import Language, Track, Playlist, Topic
 
-class AudioType(DjangoObjectType):
+class LanguageType(DjangoObjectType):
     class Meta:
-        model = Audio
+        model = Language
+        fields = ("name", "audio_url", "published")
+
+class TrackType(DjangoObjectType):
+    class Meta:
+        model = Track
+        fields = ("title", "index", "audio_url", "transcript", "duration", "created_at", "updated_at", "active", "published")
+
+class PlaylistType(DjangoObjectType):
+    class Meta:
+        model = Playlist
+        fields = ("title", "index", "audio_url", "active", "published", "tracks")
+
+class TopicType(DjangoObjectType):
+    class Meta:
+        model = Topic
+        fields=("title", "index", "audio_url", "active", "published", "playlists")
+
+class TrackInput(graphene.InputObjectType):
+    index = graphene.ID()
+    title = graphene.String()
+    audio_url = graphene.String()
+    transcript = graphene.String()
+    duration = graphene.String()
+    created_at = graphene.DateTime()
+    updated_at = graphene.DateTime()
+    active = graphene.Boolean()
+    published = graphene.Boolean()
+    
+class PlaylistInput(graphene.InputObjectType):
+    index = graphene.ID()
+    title = graphene.String()
+    audio_url = graphene.String()
+    active = graphene.Boolean()
+    published = graphene.Boolean()
+    tracks = graphene.List(TrackInput)
+
+class TopicInput(graphene.InputObjectType):
+    index = graphene.ID()
+    title = graphene.String()
+    audio_url = graphene.String()
+    active = graphene.Boolean()
+    published = graphene.Boolean()
+    playlists = graphene.List(PlaylistInput)
+
+class CreateTopic(graphene.Mutation):
+    class Arguments:
+        input = TopicInput(required=True)
+    
+    ok = graphene.Boolean()
+    topic = graphene.Field(TopicType)
+
+    @staticmethod
+    def mutate(root, info, input=None):
+        ok = True
+        playlists = []
+        for playlist_input in input.playlists:
+            playlist = Playlist.objects.get(pk=playlist_input.index)
+            if playlist is None:
+                return CreateTopic(ok=False, playlist=None)
+            playlists.append(playlist)
+        topic_instance = Topic(
+            title=input.title,
+            audio_url = input.audio_url,
+            active = input.active,
+            published = input.published,
+            )
+        topic_instance.save()
+        topic_instance.playlists.set(playlists)
+        return CreateTopic(ok=ok, topic=topic_instance)
+        
+class UpdateTopic(graphene.Mutation):
+    class Arguments:
+        index = graphene.ID(required=True)
+        title = graphene.String(required=True)
+        audio_url = graphene.String()
+
+    ok = graphene.Boolean()
+    topic = graphene.Field(TopicType)
+
+    @staticmethod
+    def mutate(root, info, index, title, audio_url=None):
+        ok = False
+        topic_instance = Topic.objects.get(pk=index)
+        if topic_instance:
+            ok = True
+            playlists = []
+            for playlist_input in input.playlists:
+                playlist = Playlist.objects.get(pk=playlist_input.index)
+                if playlist is None:
+                    return UpdateTopic(ok=False, playlist=None)
+                playlists.append(playlist)
+            topic_instance.title = title
+            topic_instance.audio_url = audio_url
+            topic_instance.save()
+            topic_instance.playlists.set(playlists)
+            return UpdateTopic(ok=ok, topic=topic_instance)
+        return UpdateTopic(ok=ok, topic=None)
+
+class DeleteTopic(graphene.Mutation):
+    class Arguments:
+        index = graphene.ID()
+    
+    ok = graphene.Boolean()
+
+    @classmethod
+    def mutate(cls, root, info, **kwargs):
+        obj = Topic.objects.get(pk=kwargs["index"])
+        obj.delete()
+        return cls(ok=True)
+
+
+class CreatePlaylist(graphene.Mutation):
+    class Arguments:
+        input = PlaylistInput(required=True)
+    
+    ok = graphene.Boolean()
+    playlist = graphene.Field(PlaylistType)
+
+    @staticmethod
+    def mutate(root, info, input=None):
+        ok = True
+        tracks = []
+        for track_input in input.tracks:
+            track = Track.objects.get(pk=track_input.index)
+            if track is None:
+                return CreatePlaylist(ok=False, playlist=None)
+            playlists.append(playlist)
+        playlist_instance = Playlist(
+            title=input.title,
+            audio_url = input.audio_url,
+            active = input.active,
+            published = input.published,
+            )
+        playlist_instance.save()
+        playlist_instance.tracks.set(tracks)
+        return CreatePlaylist(ok=ok, playlist=playlist_instance)
+        
+class UpdatePlaylist(graphene.Mutation):
+    class Arguments:
+        index = graphene.ID(required=True)
+        title = graphene.String(required=True)
+        audio_url = graphene.String()
+
+    ok = graphene.Boolean()
+    playlist = graphene.Field(PlaylistType)
+
+    @staticmethod
+    def mutate(root, info, index, title, audio_url=None):
+        ok = False
+        playlist_instance = Playlist.objects.get(pk=index)
+        if playlist_instance:
+            ok = True
+            tracks = []
+            for track_input in input.tracks:
+                track = Track.objects.get(pk=track_input.index)
+                if track is None:
+                    return UpdatePlaylist(ok=False, track=None)
+                tracks.append(track)
+            playlist_instance.title = title
+            playlist_instance.audio_url = audio_url
+            playlist_instance.save()
+            playlist_instance.tracks.set(tracks)
+            return UpdatePlaylist(ok=ok, track=track_instance)
+        return UpdatePlaylist(ok=ok, track=None)
+
+class DeletePlaylist(graphene.Mutation):
+    class Arguments:
+        index = graphene.ID()
+    
+    ok = graphene.Boolean()
+
+    @classmethod
+    def mutate(cls, root, info, **kwargs):
+        obj = Playlist.objects.get(pk=kwargs["index"])
+        obj.delete()
+        return cls(ok=True)
+
+class CreateTrack(graphene.Mutation):
+    class Arguments:
+        input = TrackInput(required=True)
+    
+    ok = graphene.Boolean()
+    playlist = graphene.Field(TrackType)
+
+    @staticmethod
+    def mutate(root, info, input=None):
+        ok = True
+        track_instance = Track(
+            title=input.title,
+            audio_url = input.audio_url,
+            transcript = input.transcript,
+            duration = input.duration,
+            created_at = graphene.DateTime(),
+            updated_at = graphene.DateTime(),
+            active = input.active,
+            published = input.published
+            )
+        track_instance.save()
+        return CreateTrack(ok=ok, track=track_instance)
+
+class UpdateTrack(graphene.Mutation):
+    class Arguments:
+        index = graphene.ID(required=True)
+        transcript = graphene.String()
+        audio_url = graphene.String()
+        duration = graphene.String()
+
+    ok = graphene.Boolean()
+    track = graphene.Field(TrackType)
+
+    @staticmethod
+    def mutate(root, info, index, duration, transcript=None, audio_url=None):
+        ok = False
+        track_instance = Track.objects.get(pk=index)
+        if track_instance:
+            ok = True
+            if audio_url:
+                track_instance.audio_url = audio_url
+            if transcript:
+                track_instance.transcript = transcript
+            if duration:
+                track_instance.duration = duration
+            track_instance.save()
+            return UpdateTrack(ok=ok)
+        return UpdateTrack(ok=ok)
+
+class DeleteTrack(graphene.Mutation):
+    class Arguments:
+        index = graphene.ID()
+    
+    ok = graphene.Boolean()
+
+    @classmethod
+    def mutate(cls, root, info, **kwargs):
+        obj = Track.objects.get(pk=kwargs["index"])
+        obj.delete()
+        return cls(ok=True)
+
+class Mutation(graphene.ObjectType):
+    create_topic = CreateTopic.Field()
+    update_topic = UpdateTopic.Field()
+    delete_topic = DeleteTopic.Field()
+    create_playlist = CreatePlaylist.Field()
+    update_playlist = UpdatePlaylist.Field()
+    delete_playlist = DeletePlaylist.Field()
+    create_track = CreateTrack.Field()
+    update_track = UpdateTrack.Field()
+    delete_track = DeleteTrack.Field()
 
 class Query(graphene.ObjectType):
-    audios = graphene.List(AudioType)
+    all_languages = graphene.List(LanguageType)
+    all_tracks = graphene.List(TrackType)
+    all_playlists = graphene.List(PlaylistType)
+    all_topics = graphene.List(TopicType)
 
-    def resolve_audios(self, info, **kwargs):
-        return Audio.objects.all()
+    # def resolve_audios(self, info, **kwargs):
+    #     return Audio.objects.all()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
