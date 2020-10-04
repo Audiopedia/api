dfb172ea8a962177c1a0bdbaaaaeb8d243df6011
Tulsi Patel <tulsipatel@Tulsis-MBP.fios-router.home>
2020-10-04 01:20:37 -0400

    queries implemented

--- audiopedia/audiopedia/schema.py
+++ audiopedia/audiopedia/schema.py
@@ -1,9 +1,9 @@
 
 import graphene
 
-import audios.schema
+import audios.schema import Query as audios_query
 
-class Query(audios.schema.Query, graphene.ObjectType):
+class Query(audios_query, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query)
--- audiopedia/audios/models.py
+++ audiopedia/audios/models.py
@@ -1,32 +1,12 @@
 from django.db import models
 
-# We want a new model...
-# We need a model that matches audiopedia:
-# So we would want a "audio" app, not links
-# Each audiofile model will contain
-"""
-* id
-* question
-* language
-* category
-* actual audio file path??
-
-from audios.models import Audio
-Audio.objects.create(question="How can cleanliness prevent sickness?", language="en", category="Cleanliness")
-Audio.objects.create(question="What drugs should I avoid to stay healthy?", language="en", category="Substance Abuse")
-Audio.objects.create(question="Which helper foods should I eat regularly?", language="en", category="Nutrition")
-
-class Audio(models.Model):
-    question = models.TextField(blank=True)
-    language = models.TextField(blank=True)
-    category = models.TextField(blank=True)
-"""
-
-
 class Language(models.Model):
     name = models.CharField(max_length=100)
     audio_url = models.URLField()
     published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
 
 class Topic(models.Model):
     title = models.CharField(max_length=200)
@@ -36,6 +16,9 @@ class Topic(models.Model):
     published = models.BooleanField(default=True)
     playlists = models.ManyToManyField(Playlist)
 
+    def __str__(self):
+        return self.title
+
 class Playlist(models.Model):
     title = models.CharField(max_length=200)
     index = models.IntegerField(unique=True)
@@ -43,6 +26,9 @@ class Playlist(models.Model):
     active = model.BooleanField(default=True)
     published = models.BooleanField(default=True)
     tracks = models.ManyToManyField(Track)
+
+    def __str__(self):
+        return self.title
 
 class Track(models.Model):
     title = models.CharField(max_length=200)
@@ -54,4 +40,7 @@ class Track(models.Model):
     updated_at = models.DateTimeField()
     active = model.BooleanField(default=True)
     published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
 
